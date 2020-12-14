002249bfce42f85c22f2f5a80c5c4f1d30991504
Adrian Resler <adrian.resler@gmail.com>
2020-12-14 08:56:12 +0100

    Add Address components to checkout order page

--- src/app/pages/sn-checkout-order/sn-checkout-order-page.component.ts
+++ src/app/pages/sn-checkout-order/sn-checkout-order-page.component.ts
@@ -1,27 +1,74 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { filter, first, map, take, takeUntil } from 'rxjs/operators';
 
 import { AccountFacade } from 'ish-core/facades/account.facade';
 import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
-import { Order } from 'ish-core/models/order/order.model';
+import { BasketView } from 'ish-core/models/basket/basket.model';
+import { HttpError } from 'ish-core/models/http-error/http-error.model';
 import { User } from 'ish-core/models/user/user.model';
+import { whenTruthy } from 'ish-core/utils/operators';
 
 @Component({
   selector: 'sn-checkout-order-page',
   templateUrl: './sn-checkout-order-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SnCheckoutOrderPageComponent implements OnInit {
-  order$: Observable<Order>;
-  loading$: Observable<boolean>;
-  /** ToDo: User data should be available by the Order, see #IS-17616 */
-  user$: Observable<User>;
+export class SnCheckoutOrderPageComponent implements OnInit, OnDestroy {
+  basket$: Observable<BasketView>;
+  basketError$: Observable<HttpError>;
+  basketLoading$: Observable<boolean>;
+  addressesError$: Observable<HttpError>;
+  addressesLoading$: Observable<boolean>;
+  currentUser$: Observable<User>;
+  nextStepRequested = false;
 
-  constructor(private checkoutFacade: CheckoutFacade, private accountFacade: AccountFacade) {}
+  /**
+   * initial basket's valid addresses in order to decide which address component should be displayed
+   */
+  validBasketAddresses$: Observable<boolean>;
+
+  private destroy$ = new Subject();
+
+  constructor(private checkoutFacade: CheckoutFacade, private accountFacade: AccountFacade, private router: Router) {}
 
   ngOnInit() {
-    this.order$ = this.checkoutFacade.selectedOrder$;
-    this.loading$ = this.checkoutFacade.basketLoading$;
-    this.user$ = this.accountFacade.user$;
+    this.basket$ = this.checkoutFacade.basket$;
+    this.basketError$ = this.checkoutFacade.basketError$;
+    this.basketLoading$ = this.checkoutFacade.basketLoading$;
+    this.addressesError$ = this.accountFacade.addressesError$;
+    this.addressesLoading$ = this.accountFacade.addressesLoading$;
+    this.currentUser$ = this.accountFacade.user$;
+
+    // determine if basket addresses are available at page start
+    this.validBasketAddresses$ = this.basket$.pipe(
+      map(basket => !!basket && !!basket.invoiceToAddress && !!basket.commonShipToAddress),
+      first()
+    );
+
+    // if all line items have been deleted go to shopping cart page
+    this.basket$
+      .pipe(
+        whenTruthy(),
+        filter(basket => !basket.lineItems || !basket.lineItems.length),
+        take(1),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => this.router.navigate(['/basket']));
+  }
+
+  /**
+   * Validates the basket and jumps to the next checkout step (Shipping)
+   * if an issue with the anonymous user address leads to a basket validation error checkout-address page is shown
+   */
+  nextStep() {
+    this.nextStepRequested = true;
+    this.checkoutFacade.continue(2);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/pages/sn-checkout-order/sn-checkout-order-page.module.ts
+++ src/app/pages/sn-checkout-order/sn-checkout-order-page.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'ish-shared/shared.module';
 
+import { SnCheckoutAddressAnonymousComponent } from './sn-checkout-address-anonymous/sn-checkout-address-anonymous.component';
+import { SnCheckoutAddressComponent } from './sn-checkout-address/sn-checkout-address.component';
 import { SnCheckoutOrderPageComponent } from './sn-checkout-order-page.component';
-import { SnCheckoutOrderComponent } from './sn-checkout-order/sn-checkout-order.component';
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [SnCheckoutOrderComponent, SnCheckoutOrderPageComponent],
+  declarations: [SnCheckoutAddressAnonymousComponent, SnCheckoutAddressComponent, SnCheckoutOrderPageComponent],
 })
 export class SnCheckoutOrderPageModule {
   static component = SnCheckoutOrderPageComponent;
